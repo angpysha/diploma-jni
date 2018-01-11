f57262482435c4f5ddfa3a1a184f195cfbbdc4ee
Andrew Petrowsky <andrew.petrowsky@outlook.com>
2018-01-11 23:48:36 +0200

    Add class for dht data

--- DHT11.h
+++ DHT11.h
@@ -6,8 +6,15 @@
 #define JNI_SENSOR_H
 
 
+#include <bits/shared_ptr.h>
+
 class Sensor {
-    
+public:
+    float Temperature;
+    float Humidity;
+
+    bool GetData(int pin,std::shared_ptr<float> temperature,std::shared_ptr<float> humidity);
+
 };
 
 
