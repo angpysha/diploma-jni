bc8ac2074d07f5bfaeec8dedfb8bdaa28b934d88
DESKTOP-KMOSMNN\Asus <andrew.petrowski@outlook.com>
2018-07-02 20:32:59 +0300

    Small fixes

--- io_github_angpysha_diploma_raspberry_Sensors_BMP180.cpp
+++ io_github_angpysha_diploma_raspberry_Sensors_BMP180.cpp
@@ -30,24 +30,12 @@ JNIEXPORT jfloatArray JNICALL Java_io_github_angpysha_diploma_1raspberry_Sensors
         (JNIEnv *env, jobject obj) {
 
     BMP180& ff = BMP180::getInstance();
-
     auto data  = ff.getDataTuple();
-
-
     jfloatArray j_result = env->NewFloatArray(3);
     jfloat result[3];
-
-   // float pressure = static_cast<float >(std::get<2>(data));
     result[0] = (jfloat) std::get<0>(data);
     result[1] = (jfloat) std::get<1>(data);
     result[2] = (jfloat) std::get<2>(data);
-
-  //  std::cout << "Temperature: " << std::get<0>(data) << "; Pressure: " << std::get<2>(data) << "; Altitude: " << std::get<1>(data) <<std::endl;
-
-  //    std::cout << "Temperature: " << result[0] << "; Pressure: " << result[2] << "; Altitude: " << result[1] << std::endl;
-//    result[0] = 1;
-//    result[1] = 2;
-//    result[2] = 2;
     env->SetFloatArrayRegion(j_result, 0, 3, result);
     return j_result;
 }
